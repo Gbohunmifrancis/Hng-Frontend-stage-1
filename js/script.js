6f0a11193b19e37db7d119540f9278df7cf910ed
Francis <francisgbohunmi@gmail.com>
2024-07-01 21:11:34 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+    const updateTime = () => {
+        const now = new Date();
+        const utcTime = now.toUTCString().split(' ')[4];
+        const day = now.toLocaleDateString('en-GB', { weekday: 'long' });
+        
+        document.getElementById('current-time').innerText = utcTime;
+        document.getElementById('current-day').innerText = day;
+    };
+
+    updateTime();
+    setInterval(updateTime, 1000); // Update every second
+});
